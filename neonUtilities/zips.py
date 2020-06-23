2a00a9f73a8b34d07cf8bd8249bdf60282b01519
= <esrh@netc.eu>
2020-06-23 11:27:44 -0400

    rate limit pausing

--- neonUtilities/zips.py
+++ neonUtilities/zips.py
@@ -12,17 +12,25 @@ class NeonObservational:
         self.rootname = data["dpID"]
         if not os.path.exists(self.rootname):
             os.makedirs(self.rootname)
-        print(f"[created root folder {self.rootname}]")
+            print(f"[created root folder {self.rootname}]")
 
         self.idxurls = self.constructIdxUrls()
         print(f"{len(self.idxurls)} files in total")
+        self.zipfiles = []
         for n,idxurl in enumerate(self.idxurls):
             print(f"Downloading zip {n+1}")
             self.downloadZips(idxurl)
-
+        print("Done downloading.")
 
     def downloadZips(self,idxurl):
-        index = json.loads(requests.get(idxurl).text)['data']['files']
+        req = requests.get(idxurl)
+        while req.headers["X-RateLimit-Remaining"]==0:
+            print("Rate limit exceeded. Consider using an api token. Pausing...")
+            time.sleep(req.headers['RetryAfter'])
+            print("Retrying")
+            req = requests.get(idxurl)
+           
+        index = json.loads(req.text)['data']['files']
         zipre = re.compile("(.*)"+self.data["package"]+"(.*)zip")
         for i in range(len(index)):
             match = zipre.match(index[i]['name'])
@@ -30,6 +38,7 @@ class NeonObservational:
                 zipidx = i
                 break
         urllib.request.urlretrieve(index[zipidx]['url'],os.path.join(self.rootname, index[zipidx]['name']))
+        self.zipfiles.append(os.path.join(os.getcwd(),self.rootname,index[zipidx]['name']))
 
 
     def constructIdxUrls(self):
@@ -43,6 +52,8 @@ class NeonObservational:
             urls.extend([basicUrl(self.data['dpID'],site,date) for date in dates])
         return urls
 
+def test():
+    data = {"dpID":"DP1.10003.001","site":["WOOD"],"sdate":"2015-07","edate":"2015-07","package":"basic"}
+    obj = NeonObservational(data)
 
-data = {"dpID":"DP1.10003.001","site":["WOOD"],"sdate":"2015-07","edate":"2015-07","package":"basic"}
-obj = NeonObservational(data)
+test()
